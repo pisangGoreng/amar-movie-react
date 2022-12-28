bc1107483f1463454eaf6b311f4c3e3ab4a0781f
endy <7ofpentacles@gmail.com>
2022-12-28 19:45:18 +0700

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline text-red-400">
+          Hello world!
+        </h1>
       </header>
     </div>
   )
